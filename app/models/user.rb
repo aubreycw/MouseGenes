767f885e75ede8ac91f5f23bd5687cbca7bad266
emilycw <emily.rose.cw@gmail.com>
2015-09-19 19:45:12 +1000

    Add users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,42 @@
 class User < ActiveRecord::Base
+  validate :ensure_session_token
+  validates :username, :password_digest, :email, :session_token, presence: true
+  validates :username, :email, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  attr_reader :password
+
+  has_many :shared_subjects
+
+  has_many :tracking_subjects
+
+  has_many :accessible_subjects, :through => :shared_subjects, :source => :tracking_subject
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by({username: username})
+    return user if (user && user.is_password?(password))
+    return nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+  
 end
+
